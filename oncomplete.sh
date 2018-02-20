ebf2f68c1a5623e0183980d402136157d053ae25
Flyers <contact@flyers-web.org>
2018-02-21 00:26:28 +0100

    filter no file to copy

--- oncomplete.sh
+++ oncomplete.sh
@@ -10,12 +10,16 @@ SRC=$3
 
 while true; do
   DIR=`dirname "$SRC"`
+  if [ $2 == 0 ]; then
+    echo `date` INFO "$1" no file to copy.
+    exit 0
+  fi
   if [ "$DIR" == "$DOWNLOAD" ]; then
-    echo `date` "INFO " "$3" moved to "$COMPLETE".
     if [ -n "$FUID" ] && [ -n "$FGID" ]; then
       chown $FUID.$FGID -R "$SRC"
     fi
     mv "$SRC" "$COMPLETE"
+    echo `date` "INFO " "$3" moved to "$COMPLETE".
     exit $?
   elif [ "$DIR" == "/" -o "$DIR" == "." ]; then
     echo `date` ERROR "$3" not under "$DOWNLOAD".
